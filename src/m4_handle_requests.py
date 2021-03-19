201ec1ba91ecd1c568a4d42babf240c3d5d50fb0
Derek Chen <dinganc@Umich.edu>
2021-03-19 00:51:33 -0700

    started m4

--- src/m4_handle_requests.py
+++ src/m4_handle_requests.py
@@ -1,14 +1,32 @@
 import sqlite3
 import json
-
+from urllib.parse import unquote
 
 db_name='d2.db'
 
 def param_decipher(url):
-    pass
+    conn=sqlite3.connect(db_name)
+    cur=conn.cursor()
+    kw=url.split('kw=')[1]
+    if 'search_course_by_keywords' in url:
+        res=search_course_by_keywords(kw,cur)
+    elif 'search_review_by_keywords' in url:
+        res=search_review_by_keywords(kw,cur)
+    elif 'search_review_by_instructor' in url:
+        res=search_review_by_instructor(kw,cur)
+    elif 'search_annotation_by_facet' in url:
+        res=search_annotation_by_facet(kw,cur)
+    else:
+        res={'msg':'error: invalid params'}
+    return json.dumps(res)
 
 def search_course_by_keywords(kw,cur):
-    pass
+    kw=unquote(kw)
+    search_keywords=kw.split()
+    sql_cmd_base="""select url,title,instructor,average_rating from courses where """
+    tail="and ".join(["""title like '%{}%'""".format(i) for i in search_keywords])
+    sql_cmd=sql_cmd_base+tail
+
 
 def search_review_by_keywords(kw,cur):
     pass
@@ -18,3 +36,5 @@ def search_review_by_instructor(kw,cur):
 
 def search_annotation_by_facet(kw,cur):
     pass
+
+param_decipher('search_course_by_keywords?kw=data%20science')
