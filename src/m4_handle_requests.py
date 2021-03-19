9884d67e8caf359718df0fc3b3a58c79f78b205b
Derek Chen <dinganc@Umich.edu>
2021-03-19 00:57:43 -0700

    started m4

--- src/m4_handle_requests.py
+++ src/m4_handle_requests.py
@@ -4,6 +4,14 @@ from urllib.parse import unquote
 
 db_name='d2.db'
 
+def read_sql_to_dict(cur,sql_cmd):
+    cur.execute(sql_cmd)
+    desc = cur.description
+    column_names = [col[0] for col in desc]
+    data = [dict(zip(column_names, row))
+            for row in cur.fetchall()]
+    return data
+
 def param_decipher(url):
     conn=sqlite3.connect(db_name)
     cur=conn.cursor()
@@ -26,10 +34,19 @@ def search_course_by_keywords(kw,cur):
     sql_cmd_base="""select url,title,instructor,average_rating from courses where """
     tail="and ".join(["""title like '%{}%'""".format(i) for i in search_keywords])
     sql_cmd=sql_cmd_base+tail
-
+    res=read_sql_to_dict(cur,sql_cmd)
+    print(res)
+    return res
 
 def search_review_by_keywords(kw,cur):
-    pass
+    kw=unquote(kw)
+    search_keywords=kw.split()
+    sql_cmd_base="""select course_url,review_date,helpful_count,rating,review from reviews where """
+    tail="and ".join(["""review like '%{}%'""".format(i) for i in search_keywords])
+    sql_cmd=sql_cmd_base+tail
+    res=read_sql_to_dict(cur,sql_cmd)
+    print(res)
+    return res
 
 def search_review_by_instructor(kw,cur):
     pass
@@ -38,3 +55,4 @@ def search_annotation_by_facet(kw,cur):
     pass
 
 param_decipher('search_course_by_keywords?kw=data%20science')
+param_decipher('search_review_by_keywords?kw=data%20science')
