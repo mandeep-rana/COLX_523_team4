ac74a978ea8e38292114822cda8c90906998a8bc
Derek Chen <dinganc@Umich.edu>
2021-03-19 00:38:00 -0700

    started m4

---
+++ src/m4_handle_requests.py
@@ -0,0 +1,20 @@
+import sqlite3
+import json
+
+
+db_name='d2.db'
+
+def param_decipher(url):
+    pass
+
+def search_course_by_keywords(kw,cur):
+    pass
+
+def search_review_by_keywords(kw,cur):
+    pass
+
+def search_review_by_instructor(kw,cur):
+    pass
+
+def search_annotation_by_facet(kw,cur):
+    pass
