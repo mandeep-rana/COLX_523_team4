699eac379db48e36e107b8b715010bcdf8f7e872
Derek Chen <dinganc@Umich.edu>
2021-03-19 01:06:38 -0700

    started m4

--- src/m4_handle_requests.py
+++ src/m4_handle_requests.py
@@ -49,10 +49,26 @@ def search_review_by_keywords(kw,cur):
     return res
 
 def search_review_by_instructor(kw,cur):
-    pass
+    kw=unquote(kw)
+    search_keywords=kw.split()
+    sql_cmd_base="""select title,instructor,average_rating,course_url,review_date,helpful_count,rating,review from reviews left join courses on reviews.course_url=courses.url where """
+    tail="or ".join(["""instructor like '%{}%'""".format(i) for i in search_keywords])
+    sql_cmd=sql_cmd_base+tail
+    res=read_sql_to_dict(cur,sql_cmd)
+    print(res)
+    return res
 
 def search_annotation_by_facet(kw,cur):
-    pass
+    kw=unquote(kw)
+    search_keywords=kw.split()
+    sql_cmd_base="""select course_url,review_date,helpful_count,rating,review from annotation left join reviews on annotation.id=reviews.id where """
+    tail="and ".join(["""{}='T'""".format(i) for i in search_keywords])
+    sql_cmd=sql_cmd_base+tail
+    res=read_sql_to_dict(cur,sql_cmd)
+    print(res)
+    return res
 
 param_decipher('search_course_by_keywords?kw=data%20science')
 param_decipher('search_review_by_keywords?kw=data%20science')
+param_decipher('search_review_by_instructor?kw=andrew')
+param_decipher('search_annotation_by_facet?kw=content%20difficulty')
